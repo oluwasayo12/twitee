6bb99ca122a395a4a7c910a07e907246702334ec
Olusayo <oluwasayo12@gmail.com>
2022-04-10 22:02:27 +0100

    connection pool limit set

--- src/v1/core/classes/class.js
+++ src/v1/core/classes/class.js
@@ -12,6 +12,7 @@ module.exports = class Functions {
             password: process.env.MYSQL_PASSWORD,
             database: process.env.MYSQL_DATABASE,
             port: '3306',
+            connectionLimit : 10,
         });
     };
 
